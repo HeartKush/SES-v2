c4a88b978ff26d0df6197416e818fd56aa368b28
Ismael Parra <iparra@smartbeemo.com>
2025-03-04 21:18:03 -0500

    new changes

--- src/react-components/Tabs.jsx
+++ src/react-components/Tabs.jsx
@@ -1,34 +1,39 @@
 import { Tabs, Tab, Card, CardBody } from "@nextui-org/react";
-import "./card.css"
+import "./card.css";
 const App = ({ picturesObject }) => {
   return (
     <div className="flex w-full md:w-7/8 flex-col">
       <Tabs
-        color=""
+        color="slate-950"
         aria-label="Options"
         classNames={{
           tabList:
-            "border-b border-divider flex-wrap mx-auto gap-0 bg-slate-500/10 w-full lg:w-fit lg:flex-nowrap",
-          cursor: "bg-[#03a9f4]",
-          panel: "",
+            "border-b border-divider flex-wrap mx-auto gap-0 bg-neutral-100 w-full lg:w-1/2 lg:flex-nowrap",
+          cursor: "bg-blue-950",
+          panel: "w-1/2 mx-auto",
           base: "",
-          tab: "w-1/2 h-10" 
+          tab: "h-10",
+          tabContent: "text-slate-950",
         }}
       >
-        {Object.entries(picturesObject).map(([tabKey, pictures], index) => (
-          <Tab className="text-lg !text-black" key={index}  title={tabKey}>
-            <Card className="bg-transparent !text-black shadow-none lg:w-4/6 mx-auto">
+        {Object.entries(picturesObject).map(([tabKey, pictures]) => (
+          <Tab className="text-lg text-black"  key={tabKey} title={tabKey}>
+            <Card className="bg-transparent shadow-none pt-4">
               <CardBody className="flex-row flex-wrap justify-evenly gap-5">
-                
-                {pictures.map((picture, pictureIndex) => (
-                  <div class="shake-vertical card-container" key={pictureIndex}>
-                    <div class="card">
-                      <div class="front-content">
+                {pictures.map((picture) => (
+                  <div
+                    className="shake-vertical card-container  hover:animate-none"
+                    key={picture.alt}
+                  >
+                    <div className="card">
+                      <div className="front-content">
                         <img src={picture.src} alt={picture.alt} />
                       </div>
-                      <div class="content px-5 py-2">
-                        <p class="heading text-center">{picture.title}</p>
-                        <p class="text sm:text-lg text-white 2xl:text-lg">{picture.description}</p>
+                      <div className="content px-5 py-4">
+                        <p className="heading text-center">{picture.title}</p>
+                        <p className="text sm:text-lg text-slate-950 2xl:text-lg">
+                          {picture.description}
+                        </p>
                       </div>
                     </div>
                   </div>
